4e62fceca4b422a5911caa13e852b8315b359050
Ngô Văn Trung <Trung2601.it@gmail.com>
2023-07-02 11:56:24 +0700

    Revert "Revert "[Update] Serving HTML Pages :sparkles""
    This reverts commit 8517dbbdd19539ec7d888775e91f252f6738c385.

--- routes/shopRouter.js
+++ routes/shopRouter.js
@@ -1,9 +1,10 @@
+const path = require('path');
 const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
     console.log('Express hello');
-    res.send('<h1>ExpressJS Server</h1>');
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
     // next(); // Allows the request to continue to the next middleware in line
 });
 
