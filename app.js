8401c493a617c69f036d4b31bce397ccfe50c9f8
Ngô Văn Trung <Trung2601.it@gmail.com>
2023-07-02 19:46:37 +0700

    [Update] Serving Files Statically :lipstick:

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const express = require('express');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRouter);
 app.use('/', shopRouter);
