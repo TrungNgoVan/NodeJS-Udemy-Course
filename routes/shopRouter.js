e130b34d5205640657c25a47fd78848be02d5da6
Ngô Văn Trung <Trung2601.it@gmail.com>
2023-07-02 13:56:04 +0700

    [Update] Using a Helper Function for Navigation :hammer:

--- routes/shopRouter.js
+++ routes/shopRouter.js
@@ -1,9 +1,11 @@
+const rootDir = require('../utils/path');
+
 const path = require('path');
 const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
