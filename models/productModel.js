7fddd5c0913b4ea8d56a4087c4850938715b69d7
Ngô Văn Trung <Trung2601.it@gmail.com>
2023-07-07 15:49:55 +0700

    [Update] Refactoring the file storage code :hammer:

--- models/productModel.js
+++ models/productModel.js
@@ -4,23 +4,38 @@ const fs = require('fs');
 const path = require('path');
 const rootPath = require('../util/path');
 
+const pathData = path.join(rootPath, 'data', 'product.json');
 
+const getProductsFromFile = cb => {
+    try {
+        const fileContent = fs.readFileSync(pathData);
+        cb(fileContent.length ? JSON.parse(fileContent) : []);
+    } catch (err) {
+        console.log('Error:', err);
+        cb([]);
+    }
+}
 class Product {
     constructor(title) {
         this.title = title;
     }
 
     save() {
-        const pathData = path.join(rootPath, 'data', 'product.json'); // Create path to file
         try {
-            const fileResult = fs.readFileSync(pathData);
-            let products = [];
-            if (fileResult.length != 0) {
-                products = JSON.parse(fileResult);
-            }
-            products.push(this);
-            fs.writeFileSync(pathData, JSON.stringify(products), (err) => {
-                console.log(err);
+            // const fileResult = fs.readFileSync(pathData);
+            // let products = [];
+            // if (fileResult.length != 0) {
+            //     products = JSON.parse(fileResult);
+            // }
+            // products.push(this);
+            // fs.writeFileSync(pathData, JSON.stringify(products), (err) => {
+            //     console.log(err);
+            // })
+            getProductsFromFile(products => {
+                products.push(this);
+                fs.writeFileSync(pathData, JSON.stringify(products), (err) => {
+                    console.log(err);
+                })
             })
         } catch (err) {
             console.log('Error:', err);
@@ -28,14 +43,7 @@ class Product {
     }
 
     static fetchAll(cb) {
-        const pathData = path.join(rootPath, 'data', 'product.json');
-        try {
-            const fileContent = fs.readFileSync(pathData);
-            cb(fileContent.length ? JSON.parse(fileContent) : []);
-        } catch (err) {
-            console.log('Error:', err);
-            cb([]);
-        }
+        getProductsFromFile(cb);
     }
 }
 
